a2ef37ce3afbbe0fce50d25312cdb63acb5e56c1
szymonKaklin <szymon.kaklin@hotmail.co.uk>
2026-02-03 22:13:58 +0000

    completed 5.2

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -5,5 +5,6 @@ import "github.com/szymonKaklin/snippetbox/internal/models"
 // Define a templateData type to act as the holding structure for
 // any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
