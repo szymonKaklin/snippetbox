ebf1ccb3e2465106882c3c394b6d426bfc1c882c
szymonKaklin <szymon.kaklin@hotmail.co.uk>
2026-02-05 13:39:03 +0000

    completed 6.2

---
+++ cmd/web/middleware.go
@@ -0,0 +1,22 @@
+package main
+
+import "net/http"
+
+func commonHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Security-Policy",
+			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+
+		w.Header().Set("Server", "Go")
+
+		// commonHeaders → servemux → application handler → servemux → commonHeaders
+
+		// Any code here will execute on the way down the chain.
+		next.ServeHTTP(w, r)
+		// Any code here will execute on the way back up the chain.
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +13,8 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return mux
+	// Pass the servemux as the 'next' parameter to the commonHeaders middleware.
+	// Because commonHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return commonHeaders(mux)
 }
